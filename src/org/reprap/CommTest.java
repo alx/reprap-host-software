066078aaf42a14600fe1cbeb8df5f577ebd7b280
sai <sai@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2005-10-04 07:51:02 +0000

    Basic comms operations
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@35 cb376a5e-1013-0410-a455-b6b1f9ac8223

---
+++ src/org/reprap/CommTest.java
@@ -0,0 +1,54 @@
+package org.reprap;
+
+import javax.comm.*;
+import java.io.*;
+
+public class CommTest {
+
+    private CommPortIdentifier mPortId = null;
+    private SerialPort mPort = null;
+
+    private void test() {
+	try {
+	    openPort("1");
+	    initPort(2400);
+	    OutputStream os = getOutputStream();
+	    String msg = "Test\r\n";
+	    os.write(msg.getBytes());
+	    closePort();
+	} catch (Exception ex) {
+	    System.out.println(ex.getMessage());
+	    System.exit(1);
+	}
+    }
+
+    public void openPort(String portName) throws NoSuchPortException, PortInUseException
+    {
+	System.out.println("Opening port " + portName);
+	mPortId = CommPortIdentifier.getPortIdentifier(portName);
+	mPort = (SerialPort)mPortId.open(portName, 30000);
+    }
+    
+    public void closePort()
+    {
+	mPort.close();
+    }
+    
+    public void initPort(int baud) throws UnsupportedCommOperationException
+    {
+	mPort.setSerialPortParams(baud,
+				  SerialPort.DATABITS_8,
+				  SerialPort.STOPBITS_1,
+				  SerialPort.PARITY_NONE );
+	mPort.setFlowControlMode(javax.comm.SerialPort.FLOWCONTROL_NONE);
+    }
+    
+    OutputStream getOutputStream() throws IOException
+    {
+	return mPort.getOutputStream();
+    }
+
+    public static void main(String[] args) {
+	new CommTest().test();
+    }
+}
