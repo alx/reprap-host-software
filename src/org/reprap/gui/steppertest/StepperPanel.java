89d46121db4d47ee1f89630b6fa3331ca159c6f5
sai <sai@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2005-12-27 03:51:52 +0000

    Start to enhance UI to do more useful stuff
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@78 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- src/org/reprap/gui/steppertest/StepperPanel.java
+++ src/org/reprap/gui/steppertest/StepperPanel.java
@@ -1,10 +1,15 @@
 package org.reprap.steppertestgui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -24,6 +29,8 @@ public class StepperPanel extends JPanel implements ChangeListener {
 	private JSlider positionRequest;      // Slider to control position
 	private JSlider positionActual;       // Slider to indicate position
 	
+	private JCheckBox torque;            // If motor is driving or not
+	
 	private GenericStepperMotor motor; 
 	private boolean moving = false;   // True if (as far as we know) the motor is seeking
 	private boolean waiting = false;  // True if already waiting for a timer to complete (so we don't start another one)
@@ -38,16 +45,64 @@ public class StepperPanel extends JPanel implements ChangeListener {
 		motor = new GenericStepperMotor(communicator, new SNAPAddress(address)); 
 		
 		this.externalSpeedSlider = externalSpeedSlider;
-		
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(new JLabel("Set " + name + " axis position"));
+	
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+                
+        add(new JLabel("Set " + name + " axis position"), c);
         positionRequest = new JSlider(JSlider.HORIZONTAL, 0, 1000, 0);
         positionRequest.addChangeListener(this);
-        add(positionRequest);
-        add(new JLabel("Actual " + name + " axis position"));
+
+        c.gridy = 1;
+        add(positionRequest, c);
+        c.gridy = 2;
+        add(new JLabel("Actual " + name + " axis position"), c);
         positionActual = new JSlider(JSlider.HORIZONTAL, 0, 1000, 0);
         positionActual.setEnabled(false);
-        add(positionActual);
+        c.gridy = 3;
+        add(positionActual, c);
+        
+        c.gridx = 1;
+        c.gridy = 1;
+        JButton calibrate = new JButton("Calibrate");
+        calibrate.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent evt) {
+        		onCalibrate();
+        	}
+        });
+        //add(calibrate, c);
+        
+        c.gridy = 2;
+        torque = new JCheckBox("Torque");
+        torque.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent evt) {
+        		onTorqueUpdate();
+        	}
+        });
+        add(torque, c);
+	}
+	
+	protected void onCalibrate() {
+		JOptionPane.showMessageDialog(null, "Calibrate not implemented yet");
+	}
+	
+	protected void onTorqueUpdate()
+	{
+		try {
+			if (torque.isSelected()) {
+				// You can't currently do this
+				torque.setSelected(false);
+			} else {
+				motor.setIdle();
+				moving = false;
+			}
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, "Could not idle motor: " + ex);
+		}
+			
 	}
 
 	public void stateChanged(ChangeEvent evt) {
@@ -86,6 +141,7 @@ public class StepperPanel extends JPanel implements ChangeListener {
 
 	private void seekToSelectedPosition() throws IOException {
 		motor.seek(externalSpeedSlider.getValue(), positionRequest.getValue());
+		torque.setSelected(true);
 		moving = true;
 		startUpdates();
 	}
@@ -98,13 +154,12 @@ public class StepperPanel extends JPanel implements ChangeListener {
 	protected void updatePosition()
 	{
 		try {
-			if (moving) {
-				setDisplayPosition();
+			setDisplayPosition();
+			if (moving)
 				startUpdates();
-			}
 		} catch (IOException ex) {
 			// Ignore these if they happen
-			System.out.println("Ignored IO exception in update");
+			System.out.println("Ignored IO exception in update: " + ex);
 		}
 	}
 
