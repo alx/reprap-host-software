263ffaca9003a482207f327656f3f7607db3373d
sai <sai@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2006-04-16 02:31:11 +0000

    Removed redundant exception handler
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@198 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- src/org/reprap/SquareTest.java
+++ src/org/reprap/SquareTest.java
@@ -4,26 +4,20 @@ import org.reprap.machines.MachineFactory;
 
 public class SquareTest {
 	
-	public static void main(String[] args) {
-		try {
-			Printer reprap = MachineFactory.create();
-			reprap.calibrate();
-			reprap.selectMaterial(0);
-			reprap.setSpeed(248);
-			reprap.setExtruderSpeed(180);
+	public static void main(String[] args) throws Exception {
+		Printer reprap = MachineFactory.create();
+		reprap.calibrate();
+		reprap.selectMaterial(0);
+		reprap.setSpeed(248);
+		reprap.setExtruderSpeed(180);
 
-			// Print a square, rotated 45 degrees
-			reprap.moveTo(20, 5, 0);
-			reprap.printTo(15, 10, 0);
-			reprap.printTo(20, 15, 0);
-			reprap.printTo(25, 10, 0);
-			reprap.printTo(20, 5, 0);
-			
-			reprap.terminate();
-		}
-		catch (Exception ex) {
-			System.out.print(ex);
-			ex.printStackTrace();
-		}
+		// Print a square, rotated 45 degrees
+		reprap.moveTo(20, 5, 0);
+		reprap.printTo(15, 10, 0);
+		reprap.printTo(20, 15, 0);
+		reprap.printTo(25, 10, 0);
+		reprap.printTo(20, 5, 0);
+		
+		reprap.terminate();
 	}
 }
