35d12105e833e8504c8f1587fd540e2ef80325aa
sai <sai@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2006-04-26 05:16:39 +0000

    Removed debugging comments
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@245 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- src/org/reprap/comms/snap/CommsLock.java
+++ src/org/reprap/comms/snap/CommsLock.java
@@ -7,26 +7,21 @@ public class CommsLock {
 	private boolean locked = false;
 	
 	synchronized public void lock() {
-		System.out.println("Lock: entered");
 		while(locked) {
-			System.out.println("Lock: waiting");
+			//System.out.println("Lock: waiting");
 			try {
 				wait();
-				System.out.println("Lock: wait complete");
 			} catch (InterruptedException e) {
 			}
 		}
 		locked = true;
-		System.out.println("Lock: finished");
 	}
 	
 	synchronized public void unlock() {
-		System.out.println("Unlock: entered");
 		if (!locked)
 			System.out.println("Warning: Calling unlock from an already unlocked state!");
 		locked = false;
 		notifyAll();
-		System.out.println("Unlock: finished");
 	}
 	
 }
