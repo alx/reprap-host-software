6be180824960c0135ac0da18193494f13e81e855
edsells <edsells@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-02-23 15:00:44 +0000

    Started to work on a function which moves the axis back and forwards to determine its repeatability
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@525 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- src/org/reprap/gui/steppertest/StepperExerciser.java
+++ src/org/reprap/gui/steppertest/StepperExerciser.java
@@ -3,18 +3,35 @@
  */
 package org.reprap.gui.steppertest;
 
+import org.reprap.Preferences;
+import org.reprap.comms.snap.SNAPAddress;
+import org.reprap.comms.Communicator;
+import org.reprap.comms.snap.SNAPCommunicator;
+import org.reprap.devices.GenericStepperMotor;
+
 /**
  * @author eD Sells
  *
  */
 public class StepperExerciser {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
-
+		final int localNodeNumber = 0;
+		final int baudRate = 19200;
+		int motorId = 3;
+		int address;
+		try
+		{
+			address = Preferences.loadGlobalInt("Axis" + motorId + "Address");
+		}catch(Exception ex)
+		{
+			System.err.println("Argh 1!");
+			return;
+		}
 		// Request Stepper speed
 		// Request Step number
 		// Request delay between strokes (for cooling)
@@ -23,19 +40,49 @@ public class StepperExerciser {
 		int stepperExcerciserRepeatabilityRuns = 2;
 		int stepperExcerciserRepeatabilityStepsPerStroke = 2000;
 		
-		for (int i = 0; i <= stepperExcerciserRepeatabilityRuns; i++)
+		SNAPAddress myAddress = new SNAPAddress(localNodeNumber);
+		Communicator communicator;
+		try
+		{		
+			communicator = new SNAPCommunicator(Preferences.loadGlobalString("Port"),
+				baudRate, myAddress);
+		}catch(Exception ex)
 		{
-			for (int j = 1; j <= stepperExcerciserRepeatabilityStepsPerStroke; j++)
-			{
-				// Go out
-			}
-			 
-			for (int k = 1; k <= stepperExcerciserRepeatabilityStepsPerStroke; k++)
-			{
-				// Come back
-			}
-			
+			System.err.println("Argh 2!");
+			return;
 		}
+		GenericStepperMotor motor;
+		try
+		{			
+			motor = new GenericStepperMotor(communicator, 
+				new SNAPAddress(address), Preferences.getGlobalPreferences(), motorId);
+		}catch(Exception ex)
+		{
+			System.err.println("Argh 3!");
+			return;
+		}
+		try
+		{		
+			motor.seek(100, 300);
+		}catch(Exception ex)
+		{
+			System.err.println("Argh 4!");
+			return;
+		}
+		
+//		for (int i = 0; i <= stepperExcerciserRepeatabilityRuns; i++)
+//		{
+//			for (int j = 1; j <= stepperExcerciserRepeatabilityStepsPerStroke; j++)
+//			{
+//				// Go out
+//			}
+//			 
+//			for (int k = 1; k <= stepperExcerciserRepeatabilityStepsPerStroke; k++)
+//			{
+//				// Come back
+//			}
+//			
+//		}
 		
 	}
 
