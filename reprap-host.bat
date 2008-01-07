2bae5b1bbb9f5bb2ddc0b49c4ad3269b1dda52f7
jmarsden <jmarsden@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-01-07 06:47:52 +0000

    Updated reprap-host.bat to use classpath to find libraries.
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@1257 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- reprap-host.bat
+++ reprap-host.bat
@@ -1,1 +1,16 @@
-java -Xmx384M -jar Reprap.jar
+rem reprap-host -- runs Reprap Java host code with an appropriate classpath
+
+rem Amount of RAM to allow Java VM to use
+set RAM_SIZE=384M
+
+rem reprap.jar file and stl file
+set REPRAP_DIR=%ProgramFiles%\Reprap
+
+rem Java3D and j3d.org libraries
+set JAVA_LIBRARY_DIR=%ProgramFiles%\Java\shared
+
+cd "%REPRAP_DIR%"
+
+java -cp "%REPRAP_DIR%\;%REPRAP_DIR%\reprap.jar;%JAVA_LIBRARY_DIR%\*" -Xmx%RAM_SIZE% org/reprap/Main
+if ERRORLEVEL 1 pause
+
