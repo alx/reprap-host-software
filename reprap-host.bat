ca7818129a1617497500487b92697ed8d88cfb5e
jmarsden <jmarsden@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-01-08 00:16:23 +0000

    Create a Windows-friendly README.txt Use rem not # in reprap-host.bat
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/host@1264 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- reprap-host.bat
+++ reprap-host.bat
@@ -9,7 +9,7 @@ set REPRAP_DIR=%ProgramFiles%\Reprap
 rem Java3D and j3d.org libraries
 set JAVA_LIBRARY_DIR=%ProgramFiles%\Java\shared
 
-# cd so we can find the reprap-wv.stl file.  Can we avoid this??
+rem cd so we can find the reprap-wv.stl file.  Can we avoid this??
 IF NOT EXIST reprap-wv.stl cd "%REPRAP_DIR%"
 java -cp ".;.\reprap.jar;%REPRAP_DIR%\;%REPRAP_DIR%\reprap.jar;%JAVA_LIBRARY_DIR%\*" -Xmx%RAM_SIZE% org/reprap/Main
 if ERRORLEVEL 1 pause
